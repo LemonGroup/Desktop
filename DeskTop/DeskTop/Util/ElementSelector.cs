cd4a73057ff8bc7e09da37a892008e47cd0e8ea4
misha <misha@MAIN_PK>
2016-11-12 20:18:18 +0300

    Created class Utils.ElementSelector

---
+++ DeskTop/DeskTop/Util/ElementSelector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskTop.Util
+{
+    /// <summary>
+    /// Позволяет выбирать пользователю элементы коллекции типа T передаваемой при создании класса
+    ///  SelectedElements - возвращает выбраные элементы 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class ElementSelector<T> : IEnumerable<ElementSelector<T>.Element>
+    {
+        protected List<Element> elements;
+
+        public IEnumerable<T> SelectedElements {
+            get { return elements.Where(e => e.Selected).Select(e => e.Value); } }
+
+        public ElementSelector(IEnumerable<T> collection)
+        {
+            elements = new List<Element>();
+            foreach (T element in collection)
+                elements.Add(new Element(element));
+        }
+
+        public IEnumerator<Element> GetEnumerator()
+        {
+            return elements.AsEnumerable().GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return elements.GetEnumerator();
+        }
+        public class Element
+        {
+            public bool Selected { get; set; }
+            public T Value;
+
+            public Element(T element)
+            {
+                Selected = false;
+                Value = element;
+            }
+        }
+    }
+}
