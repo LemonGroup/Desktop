44a36ac9f0ae3e40374a52829e330b0fe0c5eb83
misha <misha@MAIN_PK>
2016-11-14 23:44:17 +0300

    Add method Add, Add constructor takes collection persons

--- DeskTop/DeskTop/AbstractRepo.cs
+++ DeskTop/DeskTop/AbstractRepo.cs
@@ -16,15 +16,19 @@ namespace DeskTop
     /// <typeparam name="TKey"></typeparam>
     public abstract class AbstractRepo<T, TKey> where T : class
     {
-        protected AbstractRepo()
+        protected AbstractRepo(IEnumerable<T> items)
         {
-            items = new SortedList<TKey, ItemConteiner>();
+            this.items = new SortedList<TKey, ItemConteiner>();
+            foreach (T item in items)
+            {
+                TKey key = GetKey(item);
+                this.items.Add(key, new ItemConteiner(item,ItemState.Default));
+            }
             deletedKeys = new SortedSet<TKey>();
         }
         private SortedList<TKey, ItemConteiner> items;
         private SortedSet<TKey> deletedKeys;
         private IEnumerable<T> Items { get { return items.Values.Select(i=>i.Item); } }
-        protected abstract TKey GetKey(T obj);
         public T this[TKey key]
         {
             get
@@ -34,22 +38,28 @@ namespace DeskTop
             }
             set
             {
-                if (!items.ContainsKey(key)) items.Add(key, new ItemConteiner(value, ItemState.Created));
-                else
-                {
-                    items.Remove(key);
-                    items.Add(key, new ItemConteiner(value, ItemState.Updated));
-                }
+                Add(key, value);
             }
         }
 
-        protected abstract T Create(TKey key);
-        public abstract void Save();
+        public void Add(TKey key, T item)
+        {
+            if (!items.ContainsKey(key)) items.Add(key, new ItemConteiner(item, ItemState.Created));
+            else
+            {
+                items.Remove(key);
+                items.Add(key, new ItemConteiner(item, ItemState.Updated));
+            }           
+        }
+
 
         public void Delete(TKey key)
         {
             deletedKeys.Add(key);
         }
+        protected abstract T Create(TKey key);
+        public abstract void Save();
+        protected abstract TKey GetKey(T item);
         private enum ItemState
         {
             Default, Created, Updated
