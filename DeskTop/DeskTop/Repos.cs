2b2b78fa467a812c79955d2220235073da8a12fd
misha <misha@MAIN_PK>
2016-11-15 00:07:39 +0300

    convert persons to field and set default value

---
+++ DeskTop/DeskTop/Repos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeskTop.Persons;
+
+namespace DeskTop
+{
+    public static class Repos
+    {
+        public static PersonRepo Persons { get; } = new PersonRepo(new[] {new Person("default"),});
+    }
+}
