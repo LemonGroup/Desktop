6f5330f9cd50e6e78ab260930104ba0e86dbc4d3
misha <misha@MAIN_PK>
2016-11-15 00:08:44 +0300

    Add Person Form and begin work it functions

---
+++ DeskTop/DeskTop/Persons/FrmPersons.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DeskTop.Persons
+{
+    /// <summary>
+    /// Interaction logic for FrmPersons.xaml
+    /// </summary>
+    public partial class FrmPersons : Window
+    {
+        public FrmPersons()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Repos.Persons.Add(new Person("test"));
+            lstPersons.Items.Refresh();
+        }
+    }
+}
---
+++ DeskTop/DeskTop/Persons/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskTop.Persons
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            KeyWords = new List<string>();
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public List<string> KeyWords { get; private set; }
+    }
+}
