68c293428d258b3703bd11b7ad999a12164f3b93
misha <misha@MAIN_PK>
2016-11-15 00:05:46 +0300

    Add Add Save and GetKey methods

---
+++ DeskTop/DeskTop/Persons/PersonRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DeskTop.Persons;
+
+namespace DeskTop
+{
+    public class PersonRepo : AbstractRepo<Person, string>
+    {
+        protected override Person Create(string key)
+        {
+            return new Person(key);
+        }
+
+        public override void Save()
+        {
+            MessageBox.Show("Персоны сохранены (заглушка)");
+        }
+
+        protected override string GetKey(Person item)
+        {
+            return item.Name;
+        }
+
+        public PersonRepo(IEnumerable<Person> items) : base(items)
+        {
+        }
+    }
+}
