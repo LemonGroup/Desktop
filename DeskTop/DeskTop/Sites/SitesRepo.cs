3ee7d95330c802aad849f680aa168da62d4ee2f7
misha <misha@MAIN_PK>
2016-11-16 23:37:53 +0300

    -

---
+++ DeskTop/DeskTop/Sites/SitesRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DeskTop.Persons;
+
+namespace DeskTop
+{
+    public class SitesRepo : AbstractRepo<string, int>
+    {
+        private int lastKey;
+        protected override string Create(int key) { return ""; }
+
+        public override void Save()
+        {
+            MessageBox.Show("Сайты сохранены (заглушка)");
+        }
+
+        protected override int GetKey(string item)
+        {
+            return item.GetHashCode();
+        }
+
+        public SitesRepo(IEnumerable<string> items) : base(items) { }
+    }
+}
