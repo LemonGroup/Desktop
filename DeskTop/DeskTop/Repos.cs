29f48aa4923ecca61c1a0f3da8313490f6b617ea
misha <misha@MAIN_PK>
2016-11-17 23:39:35 +0300

    Добавлен репозиторий сайтов

--- DeskTop/DeskTop/Repos.cs
+++ DeskTop/DeskTop/Repos.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DeskTop.Persons;
+using DeskTop.Sites;
 
 namespace DeskTop
 {
     public static class Repos
     {
         public static PersonRepo Persons { get; } = new PersonRepo(new[] {new Person("default"),});
+        public static SitesRepo Sites { get; } = new SitesRepo(new Site[0]);
+
     }
 }
