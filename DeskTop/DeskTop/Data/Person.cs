413ab18df6a2e1fc1813fa51125a0b7da09cdd3a
misha <misha@MAIN_PK>
2016-11-19 00:29:12 +0300

    Упрастил пространства имен

--- DeskTop/DeskTop/Data/Person.cs
+++ DeskTop/DeskTop/Data/Person.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DeskTop.Persons
+namespace DeskTop
 {
     public class Person
     {
--- DeskTop/DeskTop/Data/SitesRepo.cs
+++ DeskTop/DeskTop/Data/SitesRepo.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using DeskTop.Persons;
-using DeskTop.Sites;
+
 
 namespace DeskTop
 {
